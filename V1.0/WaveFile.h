85bf075afeb5cc4f61ae8456c677dd0e6b803403
rsbarhey <rsbarhey@zone46-ed.cs.ucalgary.ca>
2015-12-07 23:56:19 -0700

    Refactored the code so it's no longer on larger input The app no longer use large amount of memory and therefore a segmentation fault is unlikely

--- V1.0/WaveFile.h
+++ V1.0/WaveFile.h
@@ -6,25 +6,24 @@ using namespace std;
 class WaveFile
 {
 public:
-        int 	chunkSize;
-        int	subChunk1Size;
-        short 	format;
-        short 	channels;
-        int   	sampleRate;
-        int   	byteRate;
-        short 	blockAlign;
-        short 	bitsPerSample;
-        int	dataSize;
+        float* ReadInput(char *filename, float *signal, int *Thesize);
+        void writeWaveFile(char* fileName, int numSamples, float *size);
+        
+        char *format;
+        int chunkSize;
+        char *chunkID;
+        char *subChunk1ID;
 
-        short*  signal;
-        int     signalSize;
-
-public:
-        void ReadInput(char *filename);
-
-private:
-        void parseFile(ifstream& in);
-        char* 	_data;
+        int16_t blockAlign;
+        int16_t bitsPerSample;
+        int16_t audioFormat;
+        int16_t numChannels;
+        int sampleRate;
+        char *subChunk2ID;
+        int dataSize;
+        short* fileData;
+        int subChunk1Size;
+        int byteRate; 
 };
 
 #endif
