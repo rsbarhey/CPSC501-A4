b2925180e5b90497326c59a8075d5a85e40f0667
rsbarhey <rsbarhey@zone46-ed.cs.ucalgary.ca>
2015-12-08 02:15:36 -0700

    First working version of the FFT

--- V2.0/main.cpp
+++ V2.0/main.cpp
@@ -19,6 +19,7 @@
 #include <math.h>
 #include <fstream>
 #include <iostream>
+#include <cstring>
 #include "WaveFile.h"
 using namespace std;
 
@@ -46,10 +47,16 @@ using namespace std;
 #define MONOPHONIC			1
 #define STEREOPHONIC		2
 
+#define SWAP(a, b) tempr = (a); (a) = (b); (b) = tempr
 
 /*  FUNCTION PROTOTYPES  *****************************************************/
 bool checkFileExist(char* fileName);
-void convolve(float x[], int N, float h[], int M, float y[], int P);
+void overlapAdd(float* inputData, int inputSize, float* impulseData, int impulseSize, float* outputData, int outputSize);
+void padSignal(float output[], float signal[], int signalLen, int size);
+void unpadSignal(float padded[], float unpadded[], int size);	
+void fft(float data[], int nn, int isign);
+void complexMultiplication(float compInput[],float compIR[],float compResult[], int size);
+void scaleFFT(float result[], int size);
 
 /******************************************************************************
  *
@@ -94,8 +101,7 @@ int main (int argc, char *argv[])
     int outputSize = inputSize + impulseSize - 1;
     float* outputData = new float[outputSize];
     
-    cout << "Convolving..." << endl;
-    convolve(inputData, inputSize, impulseData, impulseSize, outputData, outputSize);
+    overlapAdd(inputData, inputSize, impulseData, impulseSize, outputData, outputSize);
     inputWave->writeWaveFile(argv[3], outputSize, outputData);
 
     /*  End of program  */
@@ -115,17 +121,135 @@ bool checkFileExist(char* fileName)
     }
 }
 
-void convolve(float x[], int N, float h[], int M, float y[], int P) {
-	int n, m;
+void overlapAdd(float* inputData, int inputSize, float* impulseData, int impulseSize, float* outputData, int outputSize)
+{
+    int totalSize = 0;
+    int paddedTotalSize = 1;
+    totalSize = inputSize + impulseSize - 1;
+    
+    int i = 0;
+    while (paddedTotalSize < totalSize)
+    {
+        paddedTotalSize <<= 1; //times to pad    
+        i++; 
+    }
+    
+    cout << "Padded size: " << paddedTotalSize << "Exponent: " << i << endl;
+    cout << "Input size: " << inputSize << endl;
+    cout << "Impulse size: " << impulseSize << endl;
+    
+    float* compInput = new float[2*paddedTotalSize];
+    float* compImpulse = new float[2*paddedTotalSize];
+    float* compResult = new float[2*paddedTotalSize];
+    
+    padSignal(compInput, inputData, inputSize, 2*paddedTotalSize);
+    padSignal(compImpulse, impulseData, impulseSize, 2*paddedTotalSize);
+    memset(compResult, 0, 2*paddedTotalSize); // padding zeroes
+    
+    fft(compInput-1, paddedTotalSize, 1);
+    fft(compImpulse-1, paddedTotalSize, 1);
+    
+    cout << "Complex multiplication..." << endl;
+    complexMultiplication(compInput, compImpulse, compResult, paddedTotalSize);
+    
+    fft(compResult-1, paddedTotalSize, -1);
+    scaleFFT(compResult, paddedTotalSize);
+    unpadSignal(compResult, outputData, outputSize);
+}
 
-	for (n = 0; n < P; n++)
-		y[n] = 0.0;
+void padSignal(float output[], float signal[], int signalLen, int size)
+{
+    int i, k;
+    for(i = 0, k = 0; i<signalLen; i++, k+=2)
+    {
+        output[k] = signal[i];
+        output[k+1] = 0;    
+    }
+    i = k;
+    memset(output + k, 0, size -1); //adding zeroes
+}
 
-	for (n = 0; n < N; n++) {
-		for (m = 0; m < M; m++)
-		{
-			y[n+m] += x[n] * h[m];
-		}
-	}
+void unpadSignal(float padded[], float unpadded[], int size)
+{
+    int i, j;
+    for(i = 0, j = 0; i <size; i++, j+=2)
+    {
+        unpadded[i] = padded[j];    
+    }
+}
 
+void fft(float data[], int nn, int isign)
+{
+    unsigned long n, mmax, m, j, istep, i;
+    float wtemp, wr, wpr, wpi, wi, theta;
+    float tempr, tempi;
+
+    n = nn << 1;
+    j = 1;
+
+    for (i = 1; i < n; i += 2)
+    {
+        if (j > i)
+        {
+            SWAP(data[j], data[i]);
+    		   SWAP(data[j+1], data[i+1]);
+    	  }
+        m = nn;
+        while (m >= 2 && j > m)
+    	  {
+            j -= m;
+    		   m >>= 1;
+        }
+        j += m;
+    }
+
+    mmax = 2;
+    while (n > mmax)
+    {
+        istep = mmax << 1;
+        theta = isign * (6.28318530717959 / mmax);
+        wtemp = sin(0.5 * theta);
+        wpr = -2.0 * wtemp * wtemp;
+        wpi = sin(theta);
+        wr = 1.0;
+        wi = 0.0;
+        for (m = 1; m < mmax; m += 2)
+        {
+            for (i = m; i <= n; i += istep)
+            {
+                j = i + mmax;
+                tempr = wr * data[j] - wi * data[j+1];
+                tempi = wr * data[j+1] + wi * data[j];
+                data[j] = data[i] - tempr;
+                data[j+1] = data[i+1] - tempi;
+                data[i] += tempr;
+                data[i+1] += tempi;
+            }
+            wr = (wtemp = wr) * wpr - wi * wpi + wr;
+            wi = wi * wpr + wtemp * wpi + wi;
+        }
+        mmax = istep;
+    }
+}
+
+void complexMultiplication(float compInput[],float compImpulse[],float compResult[], int size)
+{
+    int i = 0;
+    for(i = 0; i<size; i++)
+    {
+        compResult[i*2] = compInput[i*2] * compImpulse[i*2] - compInput[i*2+1] * compImpulse[i*2+1];
+	     compResult[i*2+1] = compInput[i*2+1] * compImpulse[i*2] + compInput[i*2] * compImpulse[i*2+1];
+    }
+}
+
+void scaleFFT(float result[], int size)
+{
+    int i = 0;
+    for(i = 0; i < size; i++) 
+    {
+        float val = result[i*2] / (float)size;
+        result[i*2] = val;
+        val = result[i*2+1] / (float)size;
+        result[(i*2)+1] = val;
+    }
 }
